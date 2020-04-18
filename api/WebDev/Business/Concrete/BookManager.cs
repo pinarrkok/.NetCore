943baef8db027761639519ecd42e53725f61d681
pinar kopinarkok07@gmail.com <pinarkok07@gmail.com>
2020-04-19 02:16:54 +0300

    init

---
+++ api/WebDev/Business/Concrete/BookManager.cs
@@ -0,0 +1,62 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class BookManager : IBookService
+    {
+        //field veriyoruz ki erişebilelim. field: _bookDal
+        //sadece constructorda versek aşağıda erişemezdik.
+        private IBookDal _bookDal;
+
+        public BookManager(IBookDal bookDal)
+        {
+            _bookDal = bookDal;
+        }
+        public void Add(Book book)
+        {
+            // iş kodları
+            //get in içindeki "linq" kodu
+            if(_bookDal.Get(b => b.Name == book.Name && b.AuthorId == book.AuthorId)==null)
+            {
+                _bookDal.Add(book);
+            }
+            else
+            {
+                throw new Exception("Bu kitap adı zaten mevcut");
+            }
+            
+        }
+
+        public void Delete(Book book)
+        {
+            _bookDal.Delete(book);
+        }
+
+        public List<Book> GetAll()
+        {
+            return _bookDal.GetList().ToList();
+        }
+
+        public List<Book> GetByAuthorId(int id)
+        {
+            return _bookDal.GetList(b => b.AuthorId == id).ToList();
+        }
+
+        public Book GetById(int id)
+        {
+            return _bookDal.Get(b => b.Id == id);
+        }
+
+        public void Update(Book book)
+        {
+            _bookDal.Update(book);
+        }
+    }
+}
---
+++ api/WebDev/DataAccess/Concrete/EntityFramework/EfBookDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.DataAccess.EntitityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfBookDal:EfEntityRepositoryBase<Book,BookStoreContext>,IBookDal
+    {
+    }
+}
